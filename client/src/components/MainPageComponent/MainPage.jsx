df161c970e8a6fe392299d75dc0678824cba672b
Bunny <varun.verma2020@vitstudent.ac.in>
2024-01-20 21:54:38 +0530

    Styles the Adding questions section

--- client/src/components/MainPageComponent/MainPage.jsx
+++ client/src/components/MainPageComponent/MainPage.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useContext, useRef } from 'react'
 
 // Importing the css module
 import styles from './MainPage.module.css'
+import crossIcon from '../../assets/cancel.png'
+import plusIcon from '../../assets/plus.png'
 
 // Importing react toastify module
 import { ToastContainer, toast } from 'react-toastify';
@@ -47,8 +49,11 @@ const MainPage = () => {
   // making a state for active page 
   const [activePage, setActivePage] = useState('dashboard');
 
-  // State to apply active class to buttons (inside create Quiz modal )
+  // State to apply active class to buttons (inside create Quiz modal ) & stores the type of quiz
   const [activeType, setActiveType] = useState('q&a');
+
+  // making a state for storing the name of quiz  
+  const [title, setTitle] = useState('')
 
   // Function to handle first lvl modal form submission
   const handleFirstSubmit = (e) => {
@@ -56,12 +61,19 @@ const MainPage = () => {
     const payload = Object.fromEntries(formInputs);
     console.log(payload.title);
     console.log(activeType);
+    setTitle(payload.title);
   }
+
+  // function to handle lvl 2 form
+  const handleSecondSubmit = (e) => { }
 
   // making a state for showing Create Modal
   const [showModal, setShowModal] = useState(false);
 
-  // UseEffect for opening and closing the model using the html in-built functions
+  // making a state for showing Crate Quiz form modal
+  const [showCreateQuizForm, setShowCreateQuizForm] = useState(false);
+
+  // UseEffect for opening and closing the Initial model 
   const dialogRef = useRef();
   useEffect(() => {
     if (showModal) {
@@ -71,6 +83,17 @@ const MainPage = () => {
       dialogRef.current.close();     // method for closing Dialog tag Modal
     }
   }, [showModal]);
+
+  // UseEffect for opening and closing the create quiz form modal
+  const createDialogRef = useRef();
+  useEffect(() => {
+    if (showCreateQuizForm) {
+      createDialogRef.current.showModal(); // method for opening Dialog tag Modal
+    }
+    else {
+      createDialogRef.current.close();     // method for closing Dialog tag Modal
+    }
+  }, [showCreateQuizForm]);
 
   return (
     <div className={styles.mainPage}>
@@ -97,7 +120,7 @@ const MainPage = () => {
           activePage === 'analytics' && <Analytics />
         }
 
-        {/* CreateQuiz Modal */}
+        {/* Initial CreateQuiz Modal */}
 
         <dialog className={styles.createContainer} ref={dialogRef}>
           <form onSubmit={handleFirstSubmit} method='dialog' className={styles.formLayout}>
@@ -112,12 +135,65 @@ const MainPage = () => {
             </div>
             <div className={styles.btnContainer}>
               {/* add later : onClick={()=>setShowModal(false)} */}
-              <span><button className={styles.submissionBtn} onClick={()=>setShowModal(false)}>Cancel</button></span>
-              <span><button type='submit' className={`${styles.submissionBtn} ${styles.createActive}`} onClick={()=>setShowModal(false)}>Continue</button></span>
+              <span><button className={styles.submissionBtn} onClick={() => setShowModal(false)}>Cancel</button></span>
+              <span><button type='submit' className={`${styles.submissionBtn} ${styles.createActive}`} onClick={() => { setShowModal(false); setShowCreateQuizForm(true) }}>Continue</button></span>
             </div>
           </form>
         </dialog>
 
+        {/* Main Creating Quiz Modal */}
+        <dialog ref={createDialogRef}>
+          <form method='dialog' className={styles.mainCreateModal}>
+
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+              <span className={styles.qBtnContainer}>
+                <button type='button' className={styles.qBtn}>Q1 <img src={crossIcon} className={styles.cross} alt='' /> </button>
+                <button type='button' className={styles.qBtn}>Q2 <img src={crossIcon} className={styles.cross} alt='' /> </button>
+                <button type='button' className={styles.qBtn}>Q3 <img src={crossIcon} className={styles.cross} alt='' /> </button>
+                <span> <img src={plusIcon} className={styles.plus} alt='' /> </span>
+              </span>
+              <span className={styles.limit}>Max 5 Questions</span>
+            </div>
+
+            <div className={styles.questionTextInput}>
+              <input type="text" className={styles.questionText} name="questionText" id="questionText" placeholder={activeType === 'q&a' ? 'Question Here' : 'Poll Question here'} />
+            </div>
+            <div>
+              <span>Options type</span>
+              <label>
+                <input type="radio" name="optionType" id="optionType" value='Text' />
+                Text
+              </label>
+              <label>
+                <input type="radio" name="optionType" id="optionType" value='ImgURL' />
+                ImgURL
+              </label>
+              <label>
+                <input type="radio" name="optionType" id="optionType" value='Text&Img' />
+                Text & ImgURL
+              </label>
+            </div>
+            <div>
+              <div>
+                <input type="text" name="optionText" id="optionText" />
+                <input type="text" name="optionText" id="optionText" />
+                <input type="text" name="optionText" id="optionText" /> <span>Delete Icon</span>
+                <input type="text" name="optionText" id="optionText" /> <span>Delete Icon</span>
+                <button type='button' >Add Option</button>
+              </div>
+              <div>
+                <span>Timer</span>
+                <button type='button'>Off</button>
+                <button type='button'>5 seconds</button>
+                <button type='button'>10 seconds</button>
+              </div>
+              <div>
+                <button type='button'>Cancel</button>
+                <button type='Submit'>Create Quiz</button>
+              </div>
+            </div>
+          </form>
+        </dialog>
 
       </div>
 
